e60a4dc67b21e7291dfcc6c83eea2a21743fc702
AbdulazizNosirjonov <89240483+AbdulazizNosirjonov@users.noreply.github.com>
2023-01-13 15:18:43 +0500

    base

---
+++ Pods/Target Support Files/Pods-StoryboardContacts-StoryboardContactsUITests/Pods-StoryboardContacts-StoryboardContactsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StoryboardContacts_StoryboardContactsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_StoryboardContacts_StoryboardContactsUITests
+@end
